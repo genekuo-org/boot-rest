f2c80a4e2c36261f7e62805abc6f0cad939b70d1
KuoYu-chiung <genekuo@cybersoft4u.com>
2018-10-25 01:29:18 +0800

    minor change 3

--- scripts/stop-app.sh
+++ scripts/stop-app.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-#sudo status bootrest | grep -q '^bootrest start/running, process' \
-#&& [[ $? -eq 0 ]] \
-#&& sudo service bootrest stop || echo "App not started"
-sudo service bootrest stop
+sudo status bootrest | grep -q '^bootrest start/running, process' \
+&& [[ $? -eq 0 ]] \
+&& sudo service bootrest stop || echo "App not started"
