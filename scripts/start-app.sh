16d3a01e73cbc53fabcdb00a741c91d7edfa233e
KuoYu-chiung <genekuo@cybersoft4u.com>
2018-10-25 12:18:42 +0800

    Fix location of jar

--- scripts/start-app.sh
+++ scripts/start-app.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-cd /home/ec2-user/server
-sudo java -jar -Dserver.port=8080 \
-    *.jar > /dev/null 2> /dev/null < /dev/null &
+cd /home/ec2-user
+sudo java -jar -Dserver.port=8080 bootrest-1.jar > /dev/null 2> /dev/null < /dev/null &
